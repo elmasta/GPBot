158b6820c2336baa7d2a887ce98fee26a493b8bc
V_Robin <valentinrobin1@gmail.com>
2019-06-27 01:35:30 +0200

    ajax added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from app import app
 
 @app.route('/')
 @app.route('/main')
-
 def index():
+
     return render_template('main.html')
+
+@app.route('/process', methods=["POST"])
+def test():
+
+    question = request.form['question']
+    question += "a"
+    return jsonify({'question' : question})
